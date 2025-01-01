aaaf44946678d06d294538b8b06b2c030c635c94
Jesse <pokemonjm@gmail.com>
2025-01-01 10:46:15 -0500

    - error handling for books routes - fixed incorrect msg for author validation

--- routes/books.js
+++ routes/books.js
@@ -7,8 +7,8 @@ async function fetchBooks() {
         const books = await Book.findAll();
         const booksJSON = books.map(book => book.toJSON());
         return booksJSON;
-    } catch {
-        return "There was an error retrieving the books";
+    } catch (error) {
+        return error;
     }
 }
 
@@ -17,78 +17,92 @@ async function fetchBook(id) {
         const book = await Book.findByPk(id);
         const bookJSON = book.toJSON();
         return bookJSON;
-    } catch {
-        return "There was an error retrieving the book";
+    } catch (error) {
+        return error;
     }
 }
 
 async function postBook(book) {
     try {
         await Book.create(book);
-    } catch {
-        return "There was an error creating the book";
+    } catch (error) {
+        return error;
     }
 }
 
 async function updateBook(id, book) {
     try {
         await Book.update(book, { where: { id } });
-    } catch {
-        return "There was an error updating the book";
+    } catch (error) {
+        return error;
     }
 }
 
 async function deleteBook(id) {
     try {
         await Book.destroy({ where: { id } });
-    } catch {
-        return "There was an error deleting the book";
+    } catch (error) {
+        return error;
     }
 }
 
 /* GET books page. */
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     const books = await fetchBooks();
-
-    res.locals.title = "Book Listings";
-    res.locals.books = books;
+    //Error handling
+    if (books instanceof Error) {
+        return next(books);
+    }
 
     //Main book listing page
-    res.render("index");
+    res.render("index", { title: "Book Listings", books });
 });
 
 //Display new book form
 router.get('/new', (req, res) => {
-    res.locals.title = "New Book Page";
-    res.render("new-book");
+    res.render("new-book", { title: "New Book Page" });
 });
 
 //Post new book to database
 router.post('/new', async (req, res) => {
     const book = req.body;
-    await postBook(book);
+    const error = await postBook(book);
+    //Error handling
+    if (error) {
+        const errors = error.errors.map(err => { return err.message; });
+        return res.render("new-book", { errors, title: "New Book Page" });
+    }
     res.redirect('/books');
 });
 
 //Get book update form
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
     const book = await fetchBook(req.params.id);
-    res.locals.title = book.title;
-    res.locals.book = book;
-
-    res.render("update-book");
+    if (book instanceof Error) {
+        return next(book);
+    }
+    res.render("update-book", { title: book.title, book });
 });
 
 //Post updated book to database
 router.post('/:id/update', async (req, res) => {
     const book = req.body;
-    await updateBook(req.params.id, book);
+    const error = await updateBook(req.params.id, book);
+    //Error handling
+    if (error) {
+        const errors = error.errors.map(err => { return err.message; });
+        return res.render("update-book", { errors, book, title: book.title });
+    }
     res.redirect('/books');
 });
 
 //Delete book from database
-router.post('/:id/delete', async (req, res) => {
-    await deleteBook(req.params.id);
+router.post('/:id/delete', async (req, res, next) => {
+    const error = await deleteBook(req.params.id);
+    //Error handling
+    if (error) {
+        return next(error);
+    }
     res.redirect('/books');
 });
 
